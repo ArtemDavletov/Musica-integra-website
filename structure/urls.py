527bcfe55aa280fd110073f612a10ff4d84a9ee8
Artem Davletov <Tomas-Cooper@mail.ru>
2020-04-17 11:08:06 +0300

    Some changes
    Change imgages
    Add links to google docks

--- structure/urls.py
+++ structure/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('artodox/', views.artodox, name='artodox'),
     path('incubator/', views.incubator, name='incubator'),
+    path('children/', views.children, name='children'),
+    path('session/', views.session, name='session'),
 ]
--- structure/views.py
+++ structure/views.py
@@ -6,8 +6,16 @@ def main(request):
 
 
 def artodox(request):
-    return render(request, "artodox.html")
+    return render(request, "session.html")
 
 
 def incubator(request):
-    return render(request, "incubator.html")
+    return render(request, "children.html")
+
+
+def children(request):
+    return render(request, "children.html")
+
+
+def session(request):
+    return render(request, "session.html")
